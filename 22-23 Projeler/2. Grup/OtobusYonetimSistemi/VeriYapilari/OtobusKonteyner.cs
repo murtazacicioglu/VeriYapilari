67f684670bdd90f2a9912012f436f8b5fb1a68a0
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-05-11 22:20:49 +0300

    Create OtobusKonteyner.cs
    Co-Authored-By: minekorkmazz <96846332+minekorkmazz@users.noreply.github.com>
    Co-Authored-By: Fatma Büşra Tilki <120482982+fatmabusratilki@users.noreply.github.com>

---
+++ 22-23 Projeler/2. Grup/OtobusYonetimSistemi/VeriYapilari/OtobusKonteyner.cs
@@ -0,0 +1,8 @@
+﻿namespace VeriYapilari;
+
+public class OtobusKonteyner
+{
+    public string? ID => Otobus?.ID;
+    public Otobus? Otobus { get; set; }
+    public OtobusKonteyner? Siradaki { get; set; }
+}
