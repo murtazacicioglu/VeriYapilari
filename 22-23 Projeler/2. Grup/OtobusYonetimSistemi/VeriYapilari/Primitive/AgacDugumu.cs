c9ad0833d4256280542d65bb0d63f6b78445975a
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-05-11 22:20:57 +0300

    Create AgacDugumu.cs
    Co-Authored-By: minekorkmazz <96846332+minekorkmazz@users.noreply.github.com>
    Co-Authored-By: Fatma Büşra Tilki <120482982+fatmabusratilki@users.noreply.github.com>

---
+++ 22-23 Projeler/2. Grup/OtobusYonetimSistemi/VeriYapilari/Primitive/AgacDugumu.cs
@@ -0,0 +1,15 @@
+﻿namespace VeriYapilari.Primitive;
+
+public class AgacDugumu
+{
+    public int value;
+    public AgacDugumu? left;
+    public AgacDugumu? right;
+
+    public AgacDugumu(int value)
+    {
+        this.value = value;
+        left = null;
+        right = null;
+    }
+}
