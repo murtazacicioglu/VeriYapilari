3e22f929e765125ce04534572a6b70c63427864d
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-05-15 17:19:14 +0300

    tip bağımsızlaştırma

---
+++ 22-23 Projeler/2. Grup/OtobusYonetimSistemi/VeriYapilari/Primitive/TekYonluDugum.cs
@@ -0,0 +1,13 @@
+﻿namespace VeriYapilari.Primitive;
+    
+public class TekYonluStringDugumu
+{
+    public string Deger { get; set; }
+
+    public TekYonluStringDugumu? Siradaki { get; set; }
+
+    public TekYonluStringDugumu(string deger)
+    {
+        Deger = deger;
+    }
+}
