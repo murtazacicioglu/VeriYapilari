9d2f1dfa84b8cd83238f84e74fc466564ce89b53
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-05-15 18:17:12 +0300

    Create BiletListesi.cs

---
+++ 22-23 Projeler/2. Grup/OtobusYonetimSistemi/VeriYapilari/BiletListesi.cs
@@ -0,0 +1,11 @@
+﻿using VeriYapilari.Primitive;
+
+namespace VeriYapilari;
+
+public class BiletListesi : TekYonluBagliListe<BiletKonteyner>
+{
+    public new BiletKonteyner? Bas { get; set; }
+    public BiletListesi()
+    {
+    }
+}
