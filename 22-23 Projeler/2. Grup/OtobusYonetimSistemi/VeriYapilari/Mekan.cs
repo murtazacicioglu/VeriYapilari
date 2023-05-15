8566da6250d7b1415a14a0cdaf46a5944790749b
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-05-15 18:17:15 +0300

    Create Mekan.cs

---
+++ 22-23 Projeler/2. Grup/OtobusYonetimSistemi/VeriYapilari/Mekan.cs
@@ -0,0 +1,14 @@
+﻿using VeriYapilari.Primitive;
+
+namespace VeriYapilari;
+
+public class Mekan : TekYonluDugum<string>
+{
+    public string Isim { get; set; }
+    public new Mekan? Siradaki { get; set; }
+
+    public Mekan(string deger) : base(deger)
+    {
+        Isim = deger;
+    }
+}
