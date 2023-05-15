5c51804bdbd17443a2ddad7c0eab174f12ba20d9
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-05-15 18:17:21 +0300

    Update OtobusKonteyner.cs

--- 22-23 Projeler/2. Grup/OtobusYonetimSistemi/VeriYapilari/OtobusKonteyner.cs
+++ 22-23 Projeler/2. Grup/OtobusYonetimSistemi/VeriYapilari/OtobusKonteyner.cs
@@ -1,8 +1,15 @@
-﻿namespace VeriYapilari;
+﻿using VeriYapilari.Primitive;
 
-public class OtobusKonteyner
+namespace VeriYapilari;
+
+public class OtobusKonteyner : TekYonluDugum<Otobus>
 {
     public string? ID => Otobus?.ID;
     public Otobus? Otobus { get; set; }
     public OtobusKonteyner? Siradaki { get; set; }
+
+    public OtobusKonteyner(Otobus otobus) : base(otobus)
+    {
+        Otobus = otobus;
+    }
 }
