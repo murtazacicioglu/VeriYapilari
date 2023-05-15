b1c5d43c15b74f5f7b5a687c620497f625e22ea5
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-05-15 18:17:32 +0300

    Create Program.cs

---
+++ 22-23 Projeler/2. Grup/OtobusYonetimSistemi/OtobusYonetimSistemi/Program.cs
@@ -0,0 +1,12 @@
+namespace OtobusYonetimSistemi
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
