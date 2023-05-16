a38d15c074a254433bdc5592f278c4354ad603fd
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-05-16 06:10:24 +0300

    Update Program.cs

--- 22-23 Projeler/2. Grup/OtobusYonetimSistemi/TestAlani/Program.cs
+++ 22-23 Projeler/2. Grup/OtobusYonetimSistemi/TestAlani/Program.cs
@@ -14,7 +14,8 @@ var d2 = new DateTime(2023, 6, 12, 13, 30, 00);
 
 
 var o1 = new OtobusKonteyner(new Otobus("16 FYB 01", new Sefer(new AnVeMekan(m5, d1), new AnVeMekan(m2, d2)), 60));
-
+o1.Otobus.BiletAl(1);
+var b = new Bilet("ferit", "0321", 1, true, "", o1.Otobus, o1.Otobus.Sefer);
 
 var otobusler = new OtobusListesi();
 
