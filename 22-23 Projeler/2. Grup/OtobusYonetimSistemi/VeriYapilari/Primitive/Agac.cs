9574792b3f6e5da8ac90f02e53407b432a9ca9c5
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-05-16 05:43:07 +0300

    Update Agac.cs

--- 22-23 Projeler/2. Grup/OtobusYonetimSistemi/VeriYapilari/Primitive/Agac.cs
+++ 22-23 Projeler/2. Grup/OtobusYonetimSistemi/VeriYapilari/Primitive/Agac.cs
@@ -1,129 +1,128 @@
-﻿namespace VeriYapilari.Primitive
+﻿namespace VeriYapilari.Primitive;
+
+public class Agac
 {
-    public class Agac
+    public AgacDugumu? root;
+
+    public Agac()
     {
-        public AgacDugumu? root;
+        root = null;
+    }
 
-        public Agac()
+    public void Insert(int value)
+    {
+        if (root == null)
         {
-            root = null;
+            root = new AgacDugumu(value);
+            Console.WriteLine(root.Deger);
+            return;
         }
 
-        public void Insert(int value)
+        AgacDugumu current = root;
+        while (true)
         {
-            if (root == null)
+            if (value < current.Deger)
             {
-                root = new AgacDugumu(value);
-                Console.WriteLine(root.value);
-                return;
-            }
-
-            AgacDugumu current = root;
-            while (true)
-            {
-                if (value < current.value)
+                if (current.Sol == null)
                 {
-                    if (current.left == null)
-                    {
-                        current.left = new AgacDugumu(value);
-                        break;
-                    }
-                    current = current.left;
-                }
-                else
-                {
-                    if (current.right == null)
-                    {
-                        current.right = new AgacDugumu(value);
-                        break;
-                    }
-                    current = current.right;
-                }
-            }
-        }
-
-        public bool Search(int value)
-        {
-            AgacDugumu current = root;
-            while (current != null)
-            {
-                if (value == current.value)
-                    return true;
-                if (value < current.value)
-                    current = current.left;
-                else if (value > current.value)
-                    current = current.right;
-            }
-            return false;
-        }
-
-        public void Delete(int value)
-        {
-            AgacDugumu current = root;
-            AgacDugumu parent = null;
-            while (current != null)
-            {
-                if (value == current.value)
+                    current.Sol = new AgacDugumu(value);
                     break;
-                parent = current;
-                if (value < current.value)
-                    current = current.left;
-                else
-                    current = current.right;
-            }
-            if (current == null)
-                return;
-            if (current.left == null && current.right == null)
-            {
-                if (current == root)
-                    root = null;
-                else if (parent.left == current)
-                    parent.left = null;
-                else
-                    parent.right = null;
-            }
-            else if (current.left == null || current.right == null)
-            {
-                AgacDugumu child = (current.left != null) ? current.left : current.right;
-                if (current == root)
-                    root = child;
-                else if (parent.left == current)
-                    parent.left = child;
-                else
-                    parent.right = child;
+                }
+                current = current.Sol;
             }
             else
             {
-                AgacDugumu minNode = current.right;
-                AgacDugumu minNodeParent = current;
-                while (minNode.left != null)
+                if (current.Sag == null)
                 {
-                    minNodeParent = minNode;
-                    minNode = minNode.left;
+                    current.Sag = new AgacDugumu(value);
+                    break;
                 }
-                current.value = minNode.value;
-                if (minNodeParent.left == minNode)
-                    minNodeParent.left = minNode.right;
-                else
-                    minNodeParent.right = minNode.right;
+                current = current.Sag;
             }
         }
+    }
 
-        public void PrintPostorder()
+    public bool Search(int value)
+    {
+        AgacDugumu current = root;
+        while (current != null)
         {
-            Console.Write("Postorder Traversal: ");
-            PrintPostorderRecursive(root);
-            Console.WriteLine();
+            if (value == current.Deger)
+                return true;
+            if (value < current.Deger)
+                current = current.Sol;
+            else if (value > current.Deger)
+                current = current.Sag;
         }
+        return false;
+    }
 
-        private void PrintPostorderRecursive(AgacDugumu node)
+    public void Delete(int value)
+    {
+        AgacDugumu current = root;
+        AgacDugumu parent = null;
+        while (current != null)
         {
-            if (node != null)
+            if (value == current.Deger)
+                break;
+            parent = current;
+            if (value < current.Deger)
+                current = current.Sol;
+            else
+                current = current.Sag;
+        }
+        if (current == null)
+            return;
+        if (current.Sol == null && current.Sag == null)
+        {
+            if (current == root)
+                root = null;
+            else if (parent.Sol == current)
+                parent.Sol = null;
+            else
+                parent.Sag = null;
+        }
+        else if (current.Sol == null || current.Sag == null)
+        {
+            AgacDugumu child = (current.Sol != null) ? current.Sol : current.Sag;
+            if (current == root)
+                root = child;
+            else if (parent.Sol == current)
+                parent.Sol = child;
+            else
+                parent.Sag = child;
+        }
+        else
+        {
+            AgacDugumu minNode = current.Sag;
+            AgacDugumu minNodeParent = current;
+            while (minNode.Sol != null)
             {
-                PrintPostorderRecursive(node.left);
-                PrintPostorderRecursive(node.right);
-                Console.Write(node.value + " ");
+                minNodeParent = minNode;
+                minNode = minNode.Sol;
             }
+            current.Deger = minNode.Deger;
+            if (minNodeParent.Sol == minNode)
+                minNodeParent.Sol = minNode.Sag;
+            else
+                minNodeParent.Sag = minNode.Sag;
+        }
+    }
+
+    public void PrintPostorder()
+    {
+        Console.Write("Postorder Traversal: ");
+        PrintPostorderRecursive(root);
+        Console.WriteLine();
+    }
+
+    private void PrintPostorderRecursive(AgacDugumu node)
+    {
+        if (node != null)
+        {
+            PrintPostorderRecursive(node.Sol);
+            PrintPostorderRecursive(node.Sag);
+            Console.Write(node.Deger + " ");
         }
     }
 }
